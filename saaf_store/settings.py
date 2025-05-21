0acafba9d6ee13189a685bd9cae2a9530bf0ee7e
ahammedfarhanvn <ahammedfarhan743@gmail.com>
2025-05-21 18:21:04 +0530

    deploy

--- saaf_store/settings.py
+++ saaf_store/settings.py
@@ -89,3 +89,10 @@ LOGIN_REDIRECT_URL = 'dashboard'
 LOGOUT_REDIRECT_URL = 'login'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+import os
+
+SECRET_KEY = os.environ.get("SECRET_KEY", "fallback-secret-key")
+DEBUG = os.environ.get("DEBUG", "True") == "True"
+ALLOWED_HOSTS = [os.environ.get("ALLOWED_HOSTS", "127.0.0.1")]
